d998ff202b861d2bb57095f1191766c84a1ff8df
Arieza Nadya <ariezanadya@gmail.com>
2016-02-29 03:23:29 +0700

    salah vigenere 2

--- src/cryptobpcs/CryptoBPCS.java
+++ src/cryptobpcs/CryptoBPCS.java
@@ -5,6 +5,11 @@
  */
 package cryptobpcs;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author atia
@@ -16,6 +21,17 @@ public class CryptoBPCS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        BufferedImage img1 = null;
+        BufferedImage img2 = null;
+
+        try {
+            img1 = ImageIO.read(new File("C:\\Users\\User\\Pictures\\a ref\\tumblr_njke98ZPdK1rqlgm2o1_500.jpg"));
+            img2 = ImageIO.read(new File("C:\\Users\\User\\Pictures\\a ref\\tumblr_njke98ZPdK1rqlgm2o1_500-min.jpg"));
+        } catch (IOException e) {
+
+        }
+
+        PSNR.calculatePSNR(img1,img2);
     }
     
 }
